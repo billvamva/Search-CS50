c9d569c241aa302480167a6467c398d8e6decaa7
Vasileios vamvakas <48909770+billvamva@users.noreply.github.com>
2020-12-25 10:47:36 +0000

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -3,50 +3,55 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
 EMPTY = None
 
-class Board:
-    def __init__(self, state, x, o):
-        self.state = state
-        self.x = x
-        self.o = o
-    
-    def return_state(self):
-        return self.state
-    
-    def get_turn(self):
-        if self.x == self.o:
-            return X
-        else:
-            return O
+
 
 def initial_state():
     """
     Returns starting state of the board.
     """
-    s = [[EMPTY, EMPTY, EMPTY],
+    initial = [[EMPTY, EMPTY, EMPTY],
         [EMPTY, EMPTY, EMPTY],
         [EMPTY, EMPTY, EMPTY]]
-    return s
+    return initial
 
 
 def player(board):
-    return board.get_turn()
+    no_x = 0
+    no_o = 0
+    
+    if terminal(board):
+        return None
     
 
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == X:
+                no_x += 1
+            
+            if board[i][j] == O:
+                no_o += 1
+    
+    if no_x > no_o:
+        return O
+    else:
+        return X
 
+    
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    actions = []
+    actions = set()
     for i in range(3):
         for j in range(3):
-            if board.state[i][j] == None:
-                actions.append((i,j))
+            if board[i][j] == None:
+                actions.add((i,j))
     return actions
 
 
@@ -56,30 +61,42 @@ def result(board, action):
     """
     i = action[0]
     j = action[1]
-    legal = actions(board)
-    if action not in legal:
-        return 'Move is not possible' 
+
+    res_board =  copy.deepcopy(board)   
+ 
     if player(board) == X:
-        board.state[i][j] = X
-        board.x += 1
-        return board.state
+        res_board[i][j] = X
+        return res_board
     else:
-        board.state[i][j] = O
-        board.o += 1 
-        return board.state
+        res_board[i][j] = O
+        return res_board
 
 
 def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    if terminal(board):
-        if board.x > board.o:
+    set_x = set()
+    set_o = set()
+
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == X:
+                set_x.add((i,j))
+            elif board[i][j] == O:
+                set_o.add((i,j))
+    
+    set_win = {frozenset([(0,0),(1,0),(2,0)]), frozenset([(0,0), (0,1), (0,2)]), 
+    frozenset([(1,0), (1,1), (1,2)]), frozenset([(2,0), (2,1), (2,2)]), 
+    frozenset([(0,2), (1,2), (2,2)]), frozenset([(0,0), (1,1), (2,2)]), 
+    frozenset([(0,2), (1,1), (2,0)]), frozenset([(0,1), (1,1), (2,1)])}
+
+    for win in set_win:
+        if win.issubset(set_x):
             return X
-        else:
+        if win.issubset(set_o):
             return O
     return None
-
 def terminal(board):
     """
     Returns True if game is over, False otherwise.
@@ -87,32 +104,37 @@ def terminal(board):
     set_x = set()
     set_o = set()
 
+
     for i in range(3):
         for j in range(3):
-            if board.state[i][j] == X:
+            if board[i][j] == X:
                 set_x.add((i,j))
-            elif board.state[i][j] == O:
+            elif board[i][j] == O:
                 set_o.add((i,j))
+    
     set_win = {frozenset([(0,0),(1,0),(2,0)]), frozenset([(0,0), (0,1), (0,2)]), 
     frozenset([(1,0), (1,1), (1,2)]), frozenset([(2,0), (2,1), (2,2)]), 
-    frozenset([(0,0), (1,2), (2,2)]), frozenset([(0,0), (0,1), (0,2)]),
-    frozenset([(0,2), (1,2), (2,2)]), frozenset([(0,0), (0,1), (0,2)]),
-    frozenset([(0,0), (0,1), (0,2)]), frozenset([(0,0), (1,1), (2,2)]),
-    frozenset([(0,2), (1,1), (2,0)])}
+    frozenset([(0,2), (1,2), (2,2)]), frozenset([(0,0), (1,1), (2,2)]), 
+    frozenset([(0,2), (1,1), (2,0)]), frozenset([(0,1), (1,1), (2,1)])}
 
     for win in set_win:
         if win.issubset(set_x) or win.issubset(set_o):
             return True
-    return False
+    
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == None:
+                return False
+    return True
 
 def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
     if winner(board) == X:
-        return 1
+        return 10
     elif winner(board) == O:
-        return -1
+        return -10
     else:
         return 0
 
@@ -120,19 +142,47 @@ def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    def max_value(state, depth):
+        if terminal(state):
+            return utility(state) - depth
+        bestVal = float('-inf')
+        for action in actions(state):
+            actionVal = min_value(result(state,action), depth + 1) 
+            bestVal = max(bestVal, actionVal) 
+        return bestVal
+
+    def min_value(state, depth):
+        if terminal(state):
+            return utility(state) + depth
+        bestVal = float('inf')
+        for action in actions(state):
+            actionVal =  max_value(result(state, action), depth + 1)
+            bestVal = min(bestVal,actionVal)
+        return bestVal   
+
+
+    if player(board) == X:
+        bestVal = float('-inf')
+        bestAction = None
+        for action in actions(board):
+            actionVal = min_value(result(board,action), 0)
+            if  actionVal > bestVal:
+                bestVal = actionVal
+                bestAction = action
+        return bestAction
+    
+    elif player(board) == O:
+        bestVal = float('inf')
+        bestAction = None
+        for action in actions(board):
+            actionVal = max_value(result(board,action), 0)
+            if actionVal < bestVal:
+                bestVal = actionVal
+                bestAction = action
+        return bestAction
+
 
 
 if __name__ == "__main__":
-    s = initial_state()
-    board = Board(s, 0, 0)
-    board.state[0][1] = X
-    board.x += 1
-    board.state[0][0] = X
-    board.x += 1
-    board.state[0][2] = X
-    board.x += 1
+    pass
 
-    print(board.state)
-    print(terminal(board))
-    print(winner(board))
